776bf50ec3e7971b461bc172296c51c2ce68a30f
Cla Tschenett <cla@aimara.ch>
2023-01-18 20:01:45 +0100

    first test added

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,14 @@
-
+const handlerFactory = require('../controllers/handlerFactory')
 const AppError = require('../utils/appError');
+const assert = require('assert');
 
-it('should summarize all number values in an array', () => {
-  const result = AppError('Blabla', 400);
-  expect(result).to
-});
+// it('should summarize all number values in an array', () => {
+//   const result = AppError('Blabla', 400);
+//   expect(result).to
+// });
+
+
+it('should return a shipment with ID 63b576071844843a15926bca63b576071844843a15926bca', function() {
+  // eslint-disable-next-line node/no-deprecated-api
+  assert.equal(handlerFactory.getOne())
+})
